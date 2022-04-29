4fb47f7be1eeadc880c6cb872b7b21103b47c877
jacobogon <104591046+jacobogon@users.noreply.github.com>
2022-04-29 18:34:08 +0200

    noevo commit

--- src/ejercicio10contornos/Restador.java
+++ src/ejercicio10contornos/Restador.java
@@ -1,18 +1,17 @@
-package ejercicio10contornos;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package ejercicio10contornos;
 
 /**
  *
  * @author jacobo.gonzalezsuare
  */
-public class sumador {
+public class Restador {
 
-    public static float sumar(float a, float b) {
-        return a + b;
+    public static float restar(float a, float b) {
+        return a - b;
     }
 }
